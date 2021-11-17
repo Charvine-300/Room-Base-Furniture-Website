c50daff9dcad88a02d5ad2c80df2b0d4605fcf85
LordEra <marvinezenwekwe@gmail.com>
2021-11-17 01:47:42 +0100

    Responsive design modification

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,40 @@
 import './css/App.css';
+import { useState } from 'react';
 import Room from './images/logo.svg';
 import Menu from './images/icon-hamburger.svg';
 import CloseMenu from './images/icon-close.svg';
 
+
 const NavBar = () => {
+  const [turkey, setTurkey] = useState('');  
+
+
+  function showMenu() {
+    if (turkey === '') {
+      setTurkey('menuBar');
+    }
+    else {
+      setTurkey('');
+    }
+  }
+
+
   return ( 
     <div className="header">
-      <div className="header white">
+      <div className="header white" id={turkey}> 
         <div className="inner-container">
-          <img src={CloseMenu} alt="Close Menu Button" className="menu-icon"/>
+          <img src={CloseMenu} alt="Close Menu Button" className="menu-icon" onClick={showMenu} />
           <ul>
-            <li> Home </li>
-            <li> Shop </li>
-            <li> About </li>
-            <li> Contact </li>
+            <li> home </li>
+            <li> shop </li>
+            <li> about </li>
+            <li> contact </li>
           </ul>
         </div>
       </div>
       <header>
         <div className="miso-soup">
-        <img src={Menu} alt="Menu Icon" className="menu-icon" />
+        <img src={Menu} alt="Menu Icon" className="menu-icon" onClick={showMenu} />
         <img src={Room} alt="Room Base Logo" className="room-base"/>
         </div>
       </header>
--- src/Slider.js
+++ src/Slider.js
@@ -27,23 +27,62 @@ const Slider = () => {
       <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="10000">
-            <img src={image[`${0 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 1" />
+            <div className="flexbox-wrapper">
+              <img src={image[`${0 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 1" className="flex-info image-height" />
+              <div className="flex-info">
+                <section>
+                  <h1> Discover innovative <br/> ways to decorate </h1>
+                  <p> 
+                    We provide unmatched quality, comfort, and style for property owners across the country. 
+                    Our experts combine form and function in bringing your vision to life. Create a room in your 
+                    own style with our collection and make your property a reflection of you and what you love.
+                  </p>
+
+                </section>
+              </div>
+            </div>
           </div>
           <div className="carousel-item" data-bs-interval="2000">
-            <img src={image[`${1 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 2" />
+            <div className="flexbox-wrapper">
+              <img src={image[`${1 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 2" className="flex-info image-height" />  
+              <div className="flex-info">
+                <section>
+                  <h1> We are available <br/> all across the globe </h1>
+                  <p> 
+                    With stores all over the world, it's easy for you to find furniture for your home or place of business. 
+                    Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
+                    store locator. Any questions? Don't hesitate to contact us today.
+                  </p>
+                </section>
+              </div>
+            </div>
           </div>
           <div className="carousel-item">
-            <img src={image[`${2 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 3" />
+            <div className="flexbox-wrapper">
+            <img src={image[`${2 + photoSwitch}`]} className="d-block w-100" alt="Room Base Slide 3" className="flex-info image-height" />     
+              <div className="flex-info">
+                <section>
+                  <h1> Manufactured with <br/> the best materials </h1>
+                  <p> 
+                    Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+                    to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+                    experience in this industry, we understand what customers want for their home and office.
+                  </p>
+                </section>
+              </div>
+            </div>
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden"> Previous </span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        <div className="button-holder">
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden"> Previous </span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
   );
